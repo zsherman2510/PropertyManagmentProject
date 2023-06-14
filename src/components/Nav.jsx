9a6235927ad832331825a8bf12abda483c4b8a96
Zavion Sherman <zsherman2510@hotmail.com>
2023-06-13 21:36:57 -0700

    created navigation (#1)

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav className="navbar">
+      <div className="container">
+        <div className="navbar_logo">
+          <Link to="/">Property Management</Link>
+        </div>
+        <ul className='navbar_links'>
+          <li className="navbar_link">
+            <Link to="/properties">Properties</Link>
+          </li>
+          <li className="navbar_link">
+          <Link to="/tenants">Tenants</Link>
+          </li>
+          <li className="navbar_link">
+            <Link to="/payments">Payments</Link>
+          </li>
+          <li className="navbar_link">
+            <Link to="/maintenance">Maintenance</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
   )
 }
 
