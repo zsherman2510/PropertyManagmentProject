f98fc096b922bcd0478e64d7a5af79d44a5da206
Zavion Sherman <zsherman2510@hotmail.com>
2023-06-13 22:21:04 -0700

    created state management (#4)
    * created footer
    
    * made routes
    
    * created state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Dashboard from './components/Dashboard'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-        <div>app</div>
+      <Dashboard />
     </>
   )
 }
