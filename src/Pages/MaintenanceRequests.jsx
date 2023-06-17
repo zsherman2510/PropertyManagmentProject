7bdf8d13e8f7cc9d74a567658e37beedb67595ba
Zavion Sherman <zsherman2510@hotmail.com>
2023-06-16 18:11:21 -0700

    created payments and maintenance pages

--- src/Pages/MaintenanceRequests.jsx
+++ src/Pages/MaintenanceRequests.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import TableComponent from '../UIComponents/TableComponent';
 
 const MaintenanceRequests = () => {
+  const [ maintenance, setMaintenance ] = useState([]);
+  const [ error, setError ] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('data.json');
+        const { maintenanceRequests } = await response.json();
+
+        setMaintenance(maintenanceRequests);
+      } catch (error) {
+        setError(error)
+      }
+    }
+
+    fetchData();
+  }, []);
+
   return (
-    <div>MaintenanceRequests</div>
+    <div className="table-widget">
+      <div className="d-flex justify-content-between">
+      <h2>Maintenance Requests</h2>
+        <div>
+          <button className='btn btn-primary'>Add Maintenance</button>
+        </div>
+      </div>
+     
+      {maintenance.length > 0 ? (<TableComponent data={maintenance}/>) : (<p>No maintenance available</p>)}
+    </div>
   )
 }
 
--- src/Pages/Payments.jsx
+++ src/Pages/Payments.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
-
+import React, { useState, useEffect } from 'react'
+import TableComponent from '../UIComponents/TableComponent';
 const Payments = () => {
+  const [ payments, setPayments ] = useState([]);
+  const [ error, setError ] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('data.json');
+        const { payments } = await response.json();
+
+        setPayments(payments);
+      } catch (error) {
+        setError(error)
+      }
+    }
+
+    fetchData();
+  }, []);
+
   return (
-    <div>Payments</div>
+    <div className="table-widget">
+      <div className="d-flex justify-content-between">
+      <h2>Payments</h2>
+        <div>
+          <button className='btn btn-primary'>Add Payments</button>
+        </div>
+      </div>
+     
+      {payments.length > 0 ? (<TableComponent data={payments}/>) : (<p>No payments available</p>)}
+    </div>
   )
 }
 
