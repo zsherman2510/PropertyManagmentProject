78bda2d5eab1fb354003fbdd5a9005bb53288ea8
Zavion Sherman <zsherman2510@hotmail.com>
2023-06-16 18:02:39 -0700

    Tenants Page

--- src/Pages/Tenants.jsx
+++ src/Pages/Tenants.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import TableComponent from '../UIComponents/TableComponent';
 
 const Tenants = () => {
+  const [tenants, setTenants] = useState([]);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('data.json');
+        const { tenants } = await response.json();
+
+        setTenants(tenants);
+      } catch (error) {
+        setError(error)
+      }
+    }
+
+    fetchData();
+  }, []);
+
   return (
-    <div>Tenants</div>
+    <div className="table-widget">
+      <div className="d-flex justify-content-between">
+      <h2>Tenants</h2>
+        <div>
+          <button className='btn btn-primary'>Add Tenants</button>
+        </div>
+      </div>
+     
+      {tenants.length > 0 ? (<TableComponent data={tenants}/>) : (<p>No tenants available</p>)}
+    </div>
   )
 }
 
